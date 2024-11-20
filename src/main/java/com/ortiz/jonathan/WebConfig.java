da619b2c2bd3934d73d670eb069d1d2035423a75
jonathan <johnkord17@gmail.com>
2024-11-20 03:00:22 -0500

    lista y detalle de buses en react con seguridad

--- src/main/java/com/ortiz/jonathan/WebConfig.java
+++ src/main/java/com/ortiz/jonathan/WebConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.ortiz.jonathan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,4 +18,4 @@ public class WebConfig {
             }
         };
     }
-}
+}*/
--- src/main/java/com/ortiz/jonathan/controller/BusController.java
+++ src/main/java/com/ortiz/jonathan/controller/BusController.java
@@ -5,6 +5,7 @@ import com.ortiz.jonathan.services.BusService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,12 +17,14 @@ public class BusController {
         this.busService = busService;
     }
 
+    @Secured("USER")
     @GetMapping("/bus/page")
     private Page<Bus> index(@RequestParam Integer page) {
         Pageable pageable = PageRequest.of(page, 6);
         return busService.listarFutbolistas(pageable);
     }
 
+    @Secured("USER")
     @GetMapping("/bus/{id}")
     public Bus bus(@PathVariable Integer id) {
         return busService.getBus(id);
